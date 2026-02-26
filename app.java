922953f26273996f4ba659b6eb556f670012d428
shilpakevala08 <148066563+shilpakevala08@users.noreply.github.com>
2026-02-26 10:18:10 +0530

    Update app.java

--- app.java
+++ app.java
@@ -1,6 +1,9 @@
-class HelloWorld {
-    public static void main(String args[])
-    {
-        System.out.println("Hello, World");
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+
+        System.out.println("Sum = " + sum);
     }
 }
